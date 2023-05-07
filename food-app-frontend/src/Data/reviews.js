2d8b51db1101d1a187e067c5341a8120793da9b8
SamithaPrabath <samithaprabath513@gmail.com>
2023-05-07 22:18:19 +0530

    delete some files

--- food-app-frontend/src/Data/reviews.js
+++
@@ -1,9 +0,0 @@
-const reviews = [
-  { username: "venura", review: "Very good hotel", rating: 4.5, userid: "ahfbx" },
-  { username: "venura", review: "Very good hotel", rating: 4, userid: "ahfbx" },
-  { username: "venura", review: "Very good hotel", rating: 3.5, userid: "ahfbx" },
-  { username: "venura", review: "Very good hotel", rating: 2.5, userid: "ahfbx" },
-  { username: "venura", review: "Very good hotel", rating: 4.5, userid: "ahfbx" },
-];
-
-export default reviews;
